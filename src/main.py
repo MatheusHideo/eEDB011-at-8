10c7571f46543b4f34690045846fad242f77f25d
Matheus Hideo Faria Higa <higa.hideo@gmail.com>
2024-09-23 22:47:59 -0300

    :bug: fix(main): fixing imports

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.database import import_bancos
-from src.producer import  producer 
-from src.consumer import consumer
+from database import import_bancos
+from producer import  producer 
+from consumer import consumer
 import sys
 import os
 
