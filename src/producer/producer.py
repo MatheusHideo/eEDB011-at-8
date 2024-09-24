61e3aab8e91827f24dbc191bf1ae0965e1850025
Matheus Hideo Faria Higa <higa.hideo@gmail.com>
2024-09-24 19:16:33 -0300

    :sparkles: feat(producer): adding print to range

--- src/producer/producer.py
+++ src/producer/producer.py
@@ -4,23 +4,19 @@ from kafka import KafkaProducer
 from config.enviroment import reclamacoes_parquet, bootstrap_servers
 
 def main():
-    
+
     parquet_file = reclamacoes_parquet
-    
+
     producer = KafkaProducer(
         bootstrap_servers=[bootstrap_servers],
         value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
-    
     df = pd.read_parquet(parquet_file)
-    
     batch_size = 10
     for start in range(0, len(df), batch_size):
         end = min(start + batch_size, len(df))
-        print(start)
-        print(end)
+        print(f"Inserindo da linha {start} até {end}")
         batch = df.iloc[start:end]
-            
         # Convert the batch to JSON format
         batch_dict = batch.to_dict(orient='records')
         for dict in batch_dict: 
